18257107ad24f01a289503d5063faa817d43adfd
99Page <nou0ggid@gmail.com>
2023-07-13 13:32:02 +0900

    public 변경

--- Sources/cookcode-resource/Color/Color + Extension.swift
+++ Sources/cookcode-resource/Color/Color + Extension.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-extension Color {
+public extension Color {
     static let mainColor = Color("mainColor", bundle: .module)
     static let gray808080 = Color("gray.808080", bundle: .module)
     static let grayBCBCBC = Color("gray.bcbcbc", bundle: .module)
